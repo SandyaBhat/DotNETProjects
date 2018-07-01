fbe62f233c3bf4ce88258e27e6d357803de931c6
Sandya Bhat <sandya.sbhat@gmail.com>
2018-07-01 23:50:45 +0530

    second Commit

--- HelloworldApp/HelloworldApp/Default.aspx.designer.vb
+++ HelloworldApp/HelloworldApp/Default.aspx.designer.vb
@@ -12,4 +12,13 @@ Option Explicit On
 
 
 Partial Public Class _Default
+
+    '''<summary>
+    '''spnWelcome control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents spnWelcome As Global.System.Web.UI.HtmlControls.HtmlGenericControl
 End Class
--- HelloworldApp/HelloworldApp/Default.aspx.vb
+++ HelloworldApp/HelloworldApp/Default.aspx.vb
@@ -2,6 +2,6 @@
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        spnWelcome.InnerText = "Welcome Guest"
     End Sub
 End Class
