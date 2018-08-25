14f9838d671477863fd2f5856138ae96a9c9e14b
Sandya Bhat <sandya.sbhat@gmail.com>
2018-08-25 19:18:40 +0530

    Demo App Project staged

---
+++ welcomeuserapp/HelloWorld.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HelloWorld.aspx.cs" Inherits="welcomeuserapp.HelloWorld" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hello World</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       <h2>Hello World!</h2>
+       <span>Enter your name:</span>
+        <input type="text" id="txtName" runat="server" />
+        <asp:Button ID="btnsubmit"  runat="server" OnClick="btnsubmit_Click" Text="Submit" />    
+        <br />
+        <span id="spnmessage" runat="server"></span>
+    </div>
+    </form>
+</body>
+</html>
---
+++ welcomeuserapp/HelloWorld.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Net;
+using System.IO;
+
+namespace welcomeuserapp
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            string strURL;
+             
+
+           string strbaseURL= ConfigurationManager.AppSettings.Get("ServiceURL");
+           if ((txtName.Value != null) || (txtName.Value != string.Empty))
+               strURL = strbaseURL + "/" + txtName.Value.Trim();
+           else
+               strURL = strbaseURL;
+           HttpWebRequest servicerequest=(HttpWebRequest)WebRequest.Create(strURL);
+           servicerequest.Method = "GET";
+           servicerequest.ContentType = "application/json";
+           WebResponse serviceresponse = servicerequest.GetResponse();
+            Stream webstream= serviceresponse.GetResponseStream();
+            StreamReader responsereader=new StreamReader(webstream);
+            string response = responsereader.ReadLine();
+            spnmessage.InnerHtml = response.Replace("\"", "");
+
+            
+
+        }
+    }
+}
